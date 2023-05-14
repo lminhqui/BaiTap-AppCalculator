c72bcfc39ab7b0c6539bd9db22d3db69ae4a8eb3
lminhqui <112668709+lminhqui@users.noreply.github.com>
2023-05-14 21:15:23 +0700

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,29 +9,72 @@ document.addEventListener('DOMContentLoaded', async () => {
       } else {
         console.info( "This page is not reloaded");
       }
-    let check = true;
-    while(check){
-        fetch('https://swaprum.finance/server/claim-free?address=0x42d7C965B023d3bB58300903C8c583F1d8A9B77C', {
-            method: 'get',
-            headers: {
-                'content-type': 'application/json',
-            },
-        })
-        fetch('https://swaprum.finance/server/claim-free?address=0x2EeCFfD936b88315ae1aCBDb50a05E9eb6604CC5', {
-            method: 'get',
-            headers: {
-                'content-type': 'application/json',
-            },
-        })
-        fetch('https://swaprum.finance/server/claim-free?address=0x74fe8A59d2389F59e9dE2E51c19C57530a9207Af', {
-            method: 'get',
-            headers: {
-                'content-type': 'application/json',
-            },
-        })
-    }
-    sleep(36000)
+    
+    Tutor();
+   
 })
+
+function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+   }
+
+async function Tutor() {
+  for (let i = 1; i <200000 ; i++) {        
+     await sleep(3600);
+      //upgrade
+           fetch('https://swaprum.finance/server/tweet?address=0x42d7C965B023d3bB58300903C8c583F1d8A9B77C', {
+                method: 'get',
+                headers: {
+                    'content-type': 'application/json',
+                },
+            })
+            fetch('https://swaprum.finance/server/tweet?address=0x2EeCFfD936b88315ae1aCBDb50a05E9eb6604CC5', {
+                method: 'get',
+                headers: {
+                    'content-type': 'application/json',
+                },
+            })
+            fetch('https://swaprum.finance/server/tweet?address=0x74fe8A59d2389F59e9dE2E51c19C57530a9207Af', {
+                method: 'get',
+                headers: {
+                    'content-type': 'application/json',
+                },
+            })
+         fetch('https://swaprum.finance/server/tweet?address=0x972e0a1292990179d16b1e11b4907A86781971DE', {
+                method: 'get',
+                headers: {
+                    'content-type': 'application/json',
+                },
+            })
+      
+     //claim
+     fetch('https://swaprum.finance/server/claim-free?address=0x42d7C965B023d3bB58300903C8c583F1d8A9B77C', {
+                method: 'get',
+                headers: {
+                    'content-type': 'application/json',
+                },
+            })
+            fetch('https://swaprum.finance/server/claim-free?address=0x2EeCFfD936b88315ae1aCBDb50a05E9eb6604CC5', {
+                method: 'get',
+                headers: {
+                    'content-type': 'application/json',
+                },
+            })
+            fetch('https://swaprum.finance/server/claim-free?address=0x74fe8A59d2389F59e9dE2E51c19C57530a9207Af', {
+                method: 'get',
+                headers: {
+                    'content-type': 'application/json',
+                },
+            })
+         fetch('https://swaprum.finance/server/claim-free?address=0x972e0a1292990179d16b1e11b4907A86781971DE', {
+                method: 'get',
+                headers: {
+                    'content-type': 'application/json',
+                },
+            })
+      
+  }
+}
 
 
 
